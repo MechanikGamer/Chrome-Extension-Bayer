809ad31fbfad8cf310d0e6a58d4d145ff9750411
MechanikGamer <123777101+MechanikGamer@users.noreply.github.com>
2023-03-28 14:30:56 +0200

    0.01A
    start

--- content.js
+++ content.js
@@ -42,14 +42,31 @@ let Step1 = false;
 // GlobalVariables
 let MaterialGroup;
 
-// Download Material Number from Ariba not Cristal
+// Material Number from Ariba not Cristal
 function AribaDownloadMaterial() {
-  // ...
+  let DownloadTag = document.getElementsByClassName(
+    'vaT leg-p-b-5 a-arw-concise-fg-row'
+  );
+  let DownloadChoose = DownloadTag[3];
+  let Textout = DownloadChoose.innerText;
+  let numb = Textout.match(/\d/g);
+  numb = numb.join('');
+  MaterialGroup = numb; // Variable for MaterialGroup
+  console.log(MaterialGroup);
+  Step1 = true;
 }
 
 function checkURL() {
-  // ...
-}
+    if (window.location.href.includes('eu.ariba.com')) {
+      AribaDownloadMaterial();
+      chrome.storage.sync.set({ materialGroup: MaterialGroup });
+    } else if (window.location.href.includes('www.howdoc.no')) {
+      chrome.storage.sync.get(['materialGroup'], function (data) {
+        // Paste the MaterialGroup value wherever needed on www.howdoc.no
+        console.log('Material Group:', data.materialGroup);
+      });
+    }
+  }
 
 checkURL();
 
